bdb5c2bf23a58d54fabcb43cdc7478a0e2f3f398
Anderson <anderson@ferneda.com.br>
2018-09-24 22:38:00 -0300

    Aula 09

---
+++ exercicios/ex024.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python36
+
+cidade = str(input('Em que cidade você nasceu? ')).strip().upper()
+print(cidade[:5].upper() == 'SANTO')
+
---
+++ exercicios/ex025.py
@@ -0,0 +1,4 @@
+#!/usr/bin/python36
+nome = str(input('Qual é o seu nome completo? ')).strip().upper()
+#print(nome)
+print('Seu nome tem Silva: {}'.format('SILVA' in nome))
