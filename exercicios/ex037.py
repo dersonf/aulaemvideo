2d332d14e911817a1e190ddf0be4a891bb0effc9
Anderson <anderson@ferneda.com.br>
2018-10-05 23:13:19 -0300

    Aula 12

--- exercicios/ex037.py
+++ exercicios/ex037.py
@@ -1,16 +1,17 @@
 #!/usr/bin/python36
-#import binary_repr
 print('='*20)
 print('Conversor binário, octal, hexadecimal')
 print('='*20)
 numero = int(input('Digite um número inteiro: '))
-print('Digite 1 para converter em binário.')
-print('Digite 2 para converter em octal.')
-print('Digite 3 para converter em hexadecimal')
+print('''Digite 1 para converter em binário.
+Digite 2 para converter em octal.
+Digite 3 para converter em hexadecimal''')
 opcao = int(input('Digite a opção desejada: '))
 if opcao == 1:
-#    binary_repr(15)
-    binario = bin(numero)
-    print('O número {} em binário é {}'.format(numero, binario[2:]))
+    print('O número {} em binário é {}'.format(numero, bin(numero)[2:]))
+elif opcao == 2:
+    print('O número {} em octal é {}'.format(numero, oct(numero)[2:]))
+elif opcao == 3:
+    print('O número {} em hexadecimal é {}'.format(numero, hex(numero)[2:].upper()))
 else:
-    print('Ainda em contrução :(')
+    print('O animal, é pra escolher entre 1 e 3, não sabe ler?')
