fa6ee48c0a7ecd076ef5715e55a3d4458e4ed27e
Anderson <anderson@ferneda.com.br>
2018-12-14 23:45:20 -0200

    Aula 19

--- exercicios/ex090.py
+++ exercicios/ex090.py
@@ -1,9 +1,14 @@
 #!/usr/bin/python36
-nome = []
-nota1 = []
-nota2 = []
-alunos = [nome, nota1, nota2]
-while True:
-    nome.append()
-nome = str(input)
+aluno = {}
+aluno['nome'] = str(input('Nome: '))
+aluno['media'] = float(input('Média: '))
+if aluno['media'] <= 4:
+    aluno['situacao'] = 'REPROVADO'
+elif aluno['media'] <= 6.5:
+    aluno['situacao'] = 'RECUPERAÇÃO'
+else:
+    aluno['situacao'] = 'APROVADO'
+print('-='*20)
+for k, v in aluno.items():
+    print(f'- {k} é igual a {v}')
 
