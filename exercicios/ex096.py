9eecd9f5e70ceea3825f56c7a5fe4a207cc1b720
Anderson <anderson@ferneda.com.br>
2018-12-30 20:49:31 -0200

    Aula 20

---
+++ exercicios/ex096.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python36
+def area():
+    a = largura * comprimento
+    print(f'A área de um terrano {largura}x{comprimento} é de {a}m².')
+
+
+largura = float(input('LARGURA (m) :'))
+comprimento = float(input('COMPRIMENTO (m) :'))
+area()
+
